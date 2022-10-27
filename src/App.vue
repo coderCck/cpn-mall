c0502217393cfab1735043b1be9b5b3a2ada09c7
姜姜 <jiangjiang980327@163.com>
2022-10-27 22:11:18 +0800

    ..

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="app">
-    
+    <router-view />
+    <!-- <hello-world /> -->
   </div>
 </template>
 
 <script>
+import HelloWorld from '@/components/HelloWorld.vue'
 
 
 export default {
+  components: { HelloWorld },
   name: 'App',
- 
+ comments:{
+   HelloWorld
+ },
+ mounted(){
+
+ }
 }
 </script>
 
---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+//路由文件
+import Vue from "vue";
+
+import VueRouter from "vue-router";
+
+
+Vue.use(VueRouter);
+
+export const routerMap = [
+    {
+        path:"./",
+        components: () => import("@/components/HelloWorld")
+    }
+
+]
+// const Home = { template: '<div>Home</div>' }
+// const About = { template: '<div>About</div>' }
+
+// const routes = [
+//     { path: '/', component: Home },
+//     { path: '/about', component: About },
+//   ]
+
+//   const approuter = router.createRouter({
+//     // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+//     history: router.createWebHashHistory(),
+//     routes, // `routes: routes` 的缩写
+//   })
+let router = new VueRouter({
+    routers:routerMap
+});
+
+  export default router;
